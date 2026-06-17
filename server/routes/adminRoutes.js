0376b6dec6ad40a341b8db4aeb2fba74b5bf0b93
Bhadri Prabhu K <bhadrikeshav0612@gmail.com>
2026-06-18 02:12:27 +0530

    fix(server): add avatarStats endpoint route

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,9 +1,10 @@
-import { getAllUsers, getAPIStats } from "../controllers/adminController.js";
+import { getAllUsers, getAPIStats, getAvatarStat } from "../controllers/adminController.js";
 
 import express from "express";
 const router = express.Router();
 
 router.get("/allChat", getAllUsers);
 router.get("/apiStats", getAPIStats);
+router.get("/avatarStats", getAvatarStat);
 
 export default router;
